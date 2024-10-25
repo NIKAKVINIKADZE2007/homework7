4eb0192300f881f09e50874351c0ab3523a74a2f
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-10-26 00:55:58 +0400

    fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import logo from '../assets/index';
-import { log } from 'console';
+
 const Header = () => {
   const pathname = usePathname();
 
